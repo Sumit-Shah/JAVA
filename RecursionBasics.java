803dd024151e6be204d5211fc59b1d94b8afc316
sumit <sumitshah001122@gmail.com>
2023-11-27 08:13:13 +0545

    Recursion Completed

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -167,4 +167,160 @@ public class RecursionBasics {
 
 
 
+
+
+
+
+
+        //PRINT x TO THE POWER N
+        // public static int power(int x, int n) {
+        //     if(n == 0) {
+        //         return 1;
+        //     }
+        //     int xnm1 = power(x, n-1);
+        //     int xn = x * xnm1;
+        //     return xn;
+
+        //     // return x  *  power(x, n-1);
+        // }
+        // public static void main(String [] args) {
+        //     System.out.println(power(2, 10));
+
+        // }
+
+
+
+
+        // public class PowerCalculator {
+
+        //     public static int power(int x, int n) {
+        //         if (n == 0) {
+        //             return 1;
+        //         } else {
+        //             return x * power(x, n - 1);
+        //         }
+        //     }
+        
+        //     public static void main(String[] args) {
+        //         System.out.println(power(2, 10));
+        //     }
+        // }
+
+
+
+
+
+
+
+        //TILING PROBLEMS
+        // public static int tilingProblem(int n) {
+        //     //base case
+        //     if(n == 0 || n == 1) {
+        //         return 1;
+        //     }
+
+        //     //kam
+        //     //vertical choice 
+        //     int fnm1 = tilingProblem(n-1);
+
+        //     //Horizontal choice
+        //     int fnm2 = tilingProblem(n-2);
+
+        //     int totWays = fnm1 + fnm2;
+        //     return totWays;
+        // }
+        // public static void main(String args[]) {
+        //     System.out.println(tilingProblem(5));
+
+        // }
+
+        // public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        //     if(idx == str.length()) {
+        //         System.out.println(newStr);
+        //         return;
+        //     }
+
+        //     //kaam
+        //     char currChar = str.charAt(idx);
+        //     if(map[currChar-'a'] == true) {
+        //         //duplicate
+        //         removeDuplicates(str, idx+1, newStr, map);
+        //     } else {
+        //         map[currChar-'a'] = true;   
+        //         removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        //     }
+        // }
+
+        // public static void main(String[] args){
+        //     String str = "Suumiit";
+        //     removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        // }
+
+
+
+
+
+
+
+        //FRIENDS PAIRING PROBLEM
+        // public static int friendsPairing(int n){
+        //     if(n == 1 || n == 2) {
+        //         return n;
+        //     }
+        //     //choice
+
+        //     // //single
+        //     // int fnm1 = friendsPairing(n-1);
+
+        //     // //pair
+        //     // int fnm2 = friendsPairing(n-2);
+        //     // int pairWays = (n-1) * fnm2;
+
+        //     // //totWays
+        //     // int totWays = fnm1 + pairWays;
+        //     // return totWays;
+
+        //     return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+        // }
+        // public static void main(String[] args) {
+        //     Scanner sc = new Scanner(System.in);
+        //     int n = sc.nextInt();
+        //     System.out.println(friendsPairing(n));
+        // }
+
+
+
+
+
+
+
+
+
+        //IMPORTANT
+        //BINARY STRINGS PROBLEM
+        //PRINT ALL BINARY STRINGS OF SIZE N WITHOUT CONSECUTIVE ONES.
+        public static void printBinStrings(int n, int lastPlace, String str) {
+            //base case
+            if(n == 0) {
+                System.out.println(str);
+                return;
+            }
+
+            //kamm
+            printBinStrings(n-1, 0, str+"0");
+
+            if(lastPlace == 0) {
+                printBinStrings(n-1, 1, str+"1");
+            }
+        }
+        public static void main(String[] args) {
+            printBinStrings(3, 0, "");
+            
+        }
+
+
+ 
+        
+        
+
 }
