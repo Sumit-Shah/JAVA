c4d454bd92966c394e401868f022fa4080287ad0
sumit <sumitshah001122@gmail.com>
2024-01-07 03:47:14 +0545

    Practise

--- BasicSorting.java
+++ BasicSorting.java
@@ -242,56 +242,59 @@
 // 2. for(int i = 0 to maxno. / range)
 
 
-import java.util.Arrays;
-import java.util.Collections;
+// import java.util.Arrays;
+// import java.util.Collections;
 
-public class BasicSorting {
-    public static void insertionSort(int arr[]) {
-        for (int i = 1; i < arr.length; i++) {
-            int curr = arr[i];
-            int prev = i - 1;
-            // Finding out the correct position to store.
-            while (prev >= 0 && arr[prev] > curr) {
-                arr[prev + 1] = arr[prev];
-                prev--;
-            }
-            // Insertion
-            arr[prev + 1] = curr;
-        }
-    }
+// public class BasicSorting {
+//     public static void insertionSort(int arr[]) {
+//         for (int i = 1; i < arr.length; i++) {
+//             int curr = arr[i];
+//             int prev = i - 1;
+//             // Finding out the correct position to store.
+//             while (prev >= 0 && arr[prev] > curr) {
+//                 arr[prev + 1] = arr[prev];
+//                 prev--;
+//             }
+//             // Insertion
+//             arr[prev + 1] = curr;
+//         }
+//     }
 
-    public static void countingSort(int arr[]) {
-        int largest = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            largest = Math.max(largest, arr[i]);
-        }
+//     public static void countingSort(int arr[]) {
+//         int largest = Integer.MIN_VALUE;
+//         for (int i = 0; i < arr.length; i++) {
+//             largest = Math.max(largest, arr[i]);
+//         }
 
-        int count[] = new int[largest + 1];
-        for (int i = 0; i < arr.length; i++) {
-            count[arr[i]]++;
-        }
+//         int count[] = new int[largest + 1];
+//         for (int i = 0; i < arr.length; i++) {
+//             count[arr[i]]++;
+//         }
 
-        // Sorting
-        int j = 0;
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
-                arr[j] = i;
-                j++;
-                count[i]--;
-            }
-        }
-    }
+//         // Sorting
+//         int j = 0;
+//         for (int i = 0; i < count.length; i++) {
+//             while (count[i] > 0) {
+//                 arr[j] = i;
+//                 j++;
+//                 count[i]--;
+//             }
+//         }
+//     }
 
-    public static void printArr(int arr[]) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-    }
+//     public static void printArr(int arr[]) {
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String args[]) {
-        int arr[] = {2, 4, 3, 8, 2, 4};
-        insertionSort(arr);
-        printArr(arr);
-    }
-}
+//     public static void main(String args[]) {
+//         int arr[] = {2, 4, 3, 8, 2, 4};
+//         insertionSort(arr);
+//         printArr(arr);
+//     }
+// }
+
+
+//Basic sorting has completed,
