20e0cd52c20d8d8fbc76a6b18afd12dc7f21c71c
Sumit Shah <sumitshah001122@gmail.com>
2024-04-02 20:40:13 +0545

    COmpleted Greedy Algorithms

--- GreedyApproach.java
+++ GreedyApproach.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Scanner;
 import java.util.*;
 public class GreedyApproach {
 
@@ -53,38 +54,244 @@ public class GreedyApproach {
 
 
 
-    //Fractional KNAPSACK Code
+    // //Fractional KNAPSACK Code
+    // public static void main(String[] args) {
+    //     int val[] = {60, 100, 120};
+    //     int weight[] = {10, 20, 30};
+    //     int W = 50;
+
+    //     double ratio[][] = new double[val.length][2];
+    //     //0th col => idx; 1st col => ratio
+
+    //     for(int i=0; i<val.length; i++) {
+    //         ratio[i][0] = i;
+    //         ratio[i][1] = val[i]/(double)weight[i];
+    //     }
+
+    //     //ascending order
+    //     Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+    //     int capacity = W;
+    //     int finalVal = 0;
+    //     for(int i=ratio.length-1; i>=0; i--) {
+    //         int idx = (int)ratio[i][0];
+    //         if(capacity >= weight[idx]) { //include full item
+    //             finalVal += val[idx];
+    //             capacity = 0;
+    //             break;
+
+    //         }
+    //     }
+
+    //     System.out.println("final value = " + finalVal);
+
+
+    // }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    //Min ABSOLUTE DIFFERENCE Pairs
+    // public static void main(String[] args) {
+    //     int A[] = {1, 2, 3};
+    //     int B[] = {2, 1, 3};
+
+    //     Arrays.sort(A);
+    //     Arrays.sort(B);
+
+    //     int minDiff = 0;
+
+    //     for(int i=0; i<A.length; i++) {
+    //         minDiff += Math.abs(A[i]-B[i]);
+    //     }
+
+    //     System.out.println("Min absolute diff of pairs = " + minDiff);
+    // }
+
+
+
+
+
+
+
+
+
+    //MAX Length CHAIN OF PAIRS.
+    // public static void main(String[] args) {
+    //     int pairs[][] = {{5,24}, {39, 60}, {5, 28}, {27,40}, {50,90}};
+
+    //     Arrays.sort(pairs, Comparator.comparingDouble(O -> O[1]));
+
+    //     int chainLen = 1;
+    //     int chainEnd = pairs[0][1]; //Last selected pair end //chain end
+
+
+    //     for(int i =1; i<pairs.length; i++) {
+    //         if(pairs[i][0] > chainEnd) {
+    //             chainLen++;
+    //             chainEnd = pairs[i][1];
+    //         }
+    //     }
+
+    //     System.out.println("Max length of chain = " + chainLen);
+    // }
+
+
+
+
+
+
+
+
+
+    // //NEPALESE Coins
+    // public static void main(String[] args) {
+    //     Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+
+    //     Arrays.sort(coins, Comparator.reverseOrder());
+
+    //     int countOfCoins = 0;
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the amount : ");
+    //     int amount = sc.nextInt();
+    //     ArrayList<Integer> ans = new ArrayList<>();
+
+    //     for (int i = 0; i < coins.length; i++) {
+    //         if (coins[i] <= amount) {
+    //             while (coins[i] <= amount) {
+    //                 countOfCoins++;
+    //                 ans.add(coins[i]);
+    //                 amount -= coins[i];
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println("Total (min) coins used = " + countOfCoins);
+
+    //     for (int i = 0; i < ans.size(); i++) {
+    //         System.out.print(ans.get(i) + " ");
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+    //JOB Sequencing Problem 
+    // static class Job {
+    //     int deadLine;
+    //     int profit;
+    //     int id;
+
+    //     public Job(int i, int d, int p) {
+    //         id = i;
+    //         deadLine = d;
+    //         profit = p;
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+    //     ArrayList<Job> jobs = new ArrayList<>();
+
+    //     for(int i=0; i<jobsInfo.length; i++) {
+    //         jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+    //     }
+
+    //     Collections.sort(jobs, (obj1, obj2) -> obj2.profit-obj1.profit);
+    //     //descending order of profit 
+
+    //     ArrayList<Integer> seq =  new ArrayList<>();
+    //     int time = 0;
+    //     for(int i=0; i<jobs.size(); i++) {
+    //         Job curr = jobs.get(i);
+    //         if(curr.deadLine > time) {
+    //             seq.add(curr.id);
+    //             time++;
+    //         }
+
+    //     }
+
+    //     //print seq
+    //     System.out.println("Max Jobs = " + seq.size());
+    //     for(int i=0; i<seq.size(); i++) {
+    //         System.out.println(seq.get(i) + " ");
+    //     }
+    //     System.out.println();
+
+
+    // }
+
+
+
+
+
+
+
+
+
+
+    //Chocola Problem
     public static void main(String[] args) {
-        int val[] = {60, 100, 120};
-        int weight[] = {10, 20, 30};
-        int W = 50;
+        int n = 4, m =6;
+        Integer costVer[] = {2, 1, 3, 1, 4}; //m-1
+        Integer costHor[] = {4, 1, 2}; //n-1
 
-        double ratio[][] = new double[val.length][2];
-        //0th col => idx; 1st col => ratio
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
 
-        for(int i=0; i<val.length; i++) {
-            ratio[i][0] = i;
-            ratio[i][1] = val[i]/(double)weight[i];
-        }
+        int h = 0, v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
 
-        //ascending order
-        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
-
-        int capacity = W;
-        int finalVal = 0;
-        for(int i=ratio.length-1; i>=0; i--) {
-            int idx = (int)ratio[i][0];
-            if(capacity >= weight[idx]) { //include full item
-                finalVal += val[idx];
-                capacity = 0;
-                break;
-
+        while(h < costHor.length && v < costVer.length) {
+            if(costVer[v] <= costHor[h]) { //horizonal cut
+                cost += (costHor[h] * vp);
+                hp++;
+                h++;
+            } else { //vertical cut
+                cost += (costVer[v] * hp);
+                vp++;
+                v++;
             }
         }
 
-        System.out.println("final value = " + finalVal);
+        while (h < costHor.length) {
+            cost += (costHor[h] * vp);
+            hp++;
+            v++;
+        }
 
+        while (v < costVer.length) {
+            cost += (costVer[v] * hp);
+            vp++;
+            v++;
 
+            
+        }
+
+        System.out.println("Min cost of cuts = " + cost);
     }
+     
+
+
+
+
+
+
     
 }
