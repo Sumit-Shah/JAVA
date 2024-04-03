81a1a0966f63d22d1d5bdf5eee7801407bcb93b6
Sumit Shah <sumitshah001122@gmail.com>
2024-04-03 21:45:55 +0545

    Tree

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -12,10 +12,10 @@ public class BinaryTreesB {
     }
 
     static class BinaryTree {
-        static int idx = -1;
-        public static Node buildTree(int nodes[]) {
+        int idx = -1; 
+        public Node buildTree(int nodes[]) {
             idx++;
-            if(nodes[idx] == -1) {
+            if(idx >= nodes.length || nodes[idx] == -1) {
                 return null;
             }
 
@@ -24,15 +24,13 @@ public class BinaryTreesB {
             newNode.right = buildTree(nodes);
 
             return newNode;
-
         }
-
     }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
-        
     }
 }
