79ae76f503bd97c21db79fa9b8c7926e44120938
Sumit Shah <sumitshah001122@gmail.com>
2024-04-07 21:53:26 +0545

    Tree 1 completed

--- GreedyApproach.java
+++ GreedyApproach.java
@@ -245,47 +245,52 @@ public class GreedyApproach {
 
 
 
-    //Chocola Problem
-    public static void main(String[] args) {
-        int n = 4, m =6;
-        Integer costVer[] = {2, 1, 3, 1, 4}; //m-1
-        Integer costHor[] = {4, 1, 2}; //n-1
+    // //Chocola Problem
+    // public static void main(String[] args) {
+    //     int n = 4, m =6;
+    //     Integer costVer[] = {2, 1, 3, 1, 4}; //m-1
+    //     Integer costHor[] = {4, 1, 2}; //n-1
 
-        Arrays.sort(costVer, Collections.reverseOrder());
-        Arrays.sort(costHor, Collections.reverseOrder());
+    //     Arrays.sort(costVer, Collections.reverseOrder());
+    //     Arrays.sort(costHor, Collections.reverseOrder());
 
-        int h = 0, v = 0;
-        int hp = 1, vp = 1;
-        int cost = 0;
+    //     int h = 0, v = 0;
+    //     int hp = 1, vp = 1;
+    //     int cost = 0;
 
-        while(h < costHor.length && v < costVer.length) {
-            if(costVer[v] <= costHor[h]) { //horizonal cut
-                cost += (costHor[h] * vp);
-                hp++;
-                h++;
-            } else { //vertical cut
-                cost += (costVer[v] * hp);
-                vp++;
-                v++;
-            }
-        }
+    //     while(h < costHor.length && v < costVer.length) {
+    //         if(costVer[v] <= costHor[h]) { //horizonal cut
+    //             cost += (costHor[h] * vp);
+    //             hp++;
+    //             h++;
+    //         } else { //vertical cut
+    //             cost += (costVer[v] * hp);
+    //             vp++;
+    //             v++;
+    //         }
+    //     }
 
-        while (h < costHor.length) {
-            cost += (costHor[h] * vp);
-            hp++;
-            v++;
-        }
+    //     while (h < costHor.length) {
+    //         cost += (costHor[h] * vp);
+    //         hp++;
+    //         v++;
+    //     }
 
-        while (v < costVer.length) {
-            cost += (costVer[v] * hp);
-            vp++;
-            v++;
+    //     while (v < costVer.length) {
+    //         cost += (costVer[v] * hp);
+    //         vp++;
+    //         v++;
 
             
-        }
+    //     }
 
-        System.out.println("Min cost of cuts = " + cost);
-    } //SPOJ
+    //     System.out.println("Min cost of cuts = " + cost);
+    // } //SPOJ
+
+
+
+
+    
      
 
 
--- heightTree.java
+++ heightTree.java
@@ -1,85 +1,163 @@
-// import java.util.*;
-
-// import javax.sound.midi.Soundbank;
-// public class heightTree {
-//     static class Node {
-//         int data;
-//         Node left, right;
-
-//         public Node(int data) {
-//             this.data = data;
-//             this.left = null;
-//             this.right = null;
-//         }
-//     }
-
-//     public static int height(Node root) {
-//         if(root == null) {
-//             return 0;
-//         }
-
-//         int lh = height(root.left);
-//         int rh = height(root.right);
-//         return Math.max(lh, rh) + 1;
-//     }
-
-
-//     public static void main(String[] args) {
-//         /* 
-//          *       1
-//          *      / \
-//          *    2    3 
-//          *   /      \
-//          * 4    5  6    7
-//          * */
-
-//          Node root = new Node(1);
-//          root.left = new Node(2);
-//          root.right = new Node(3);
-//          root.left.left = new Node(4);
-//          root.left.right = new Node(5);
-//          root.right.left = new Node (6);
-//          root.right.right = new Node(7);
-
-//          System.out.println(height(root));
-
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
 import java.util.*;
 
-public class heightTree{
-    static class TreeNode {
+import javax.swing.plaf.nimbus.State;
+public class heightTree {
+    static class Node {
         int data;
-        TreeNode left, right;
+        Node left, right;
 
-        public TreeNode(int data) {
+        public Node(int data) {
             this.data = data;
+
+             
             this.left = null;
             this.right = null;
         }
     }
 
-    public static int getHeight(TreeNode root) {
-        if (root == null) {
+
+
+
+
+
+    //HEIGHT of treee
+    public static int height(Node root) {
+        if(root == null) {
             return 0;
         }
 
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-        return Math.max(leftHeight, rightHeight) + 1;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
     }
+
+
+
+
+
+
+    //COUNT of Nodes 
+    public static int count(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+        return leftCount + rightCount + 1;
+    }
+
+
+
+
+
+    //SUM of Nodes
+    public static int sum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+
+
+
+    //DIAMETER of a Tree 
+    //Approach 1
+    public static int diameter(Node root) {   //O(n)^2
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDiam = diameter(root.left);
+        int leftHt = height(root.left);
+        int rightDiam = diameter(root.right);
+        int rightHt = height(root.right);
+
+        int selfDiam = leftHt + rightHt + 1;
+
+        return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+    }
+
+
+
+
+
+
+
+    //Approach2
+    static clas Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+    public static Info diameter1(Node root) {   //O(n)
+
+        if(root == null) {
+            return new Info(0, 0);
+        }
+        
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+
+    }
+
+
+
+
+
+
+
+
+
+    //SUBTREE of Another Tree
+    public Static boolean isIdentical(Node node, Node subRoot) {
+        if(node == null && subRoot == null) {
+            return true;
+        } else if(node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+
+        if(!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if(!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if(root == null) {
+            return false;
+        }
+        if(root.data == subRoot.data) {
+            if(isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+
+
+
+
+
 
     public static void main(String[] args) {
         /* 
@@ -90,14 +168,40 @@ public class heightTree{
          *  4   5 6   7
          * */
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(7);
+         Node root = new Node(1);
+         root.left = new Node(2);
+         root.right = new Node(3);
+         root.left.left = new Node(4);
+         root.left.right = new Node(5);
+         root.right.left = new Node (6);
+        //  root.right.right = new Node(7);
 
-        System.out.println("Height of the tree: " + getHeight(root));
+        //  System.out.println(height(root));
+
+
+        // System.out.println(count(root));
+
+
+        // System.out.println(sum(root));
+
+
+        // System.out.println(diameter(root));
+
+
+        // System.out.println(diameter1(root));
+
+
+
+
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+
+
+        System.out.println(isSubtree(root, subRoot));
+
     }
 }
+  
