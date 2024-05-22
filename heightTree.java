83233e643ca2af2e8e773df369165bf43fca98bb
Sumit Shah <sumitshah001122@gmail.com>
2024-05-22 17:57:38 +0545

    Tree part 3

--- heightTree.java
+++ heightTree.java
@@ -210,6 +210,149 @@ public class heightTree {
         System.out.println();
      }
 
+
+
+
+
+
+
+
+
+
+    //  //BINARY TREES(Part 3)
+    //  //Kth Level
+    //  public static void KLevel(Node root, int level, int k) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    
+    //     if (level == k) {
+    //         System.out.print(root.data + " ");
+    //         return;
+    //     }
+        
+    //     // Decrement the level when traversing down the tree
+    //     KLevel(root.left, level + 1, k);
+    //     KLevel(root.right, level + 1, k);
+    // }
+
+
+
+
+
+
+
+
+
+    //LOWEST COMMON ANCESTOR
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if(root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if(root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
+    }
+
+
+    public static Node lca(Node root, int n1, int n2) {  //(n)
+        ArrayList<Node> path1 =new ArrayList<>();
+        ArrayList<Node> path2 =new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        //last common ancestor
+        int i = 0;
+        for(; i<path1.size() && i<path2.size(); i++) {
+            if(path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+
+        //last equal node -> i-1th
+        Node lca = path1.get(i-1);
+        return lca;
+    }
+
+
+
+
+
+
+
+
+
+
+    //Lowest COmmon acestor 
+    public static Node lca2(Node root, int n1, int n2) {
+        if(root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+
+        //leftLCA=val rightLca = null 
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+
+
+
+
+    // MIN DISTANCE BWTWEEN NODES 
+    public static int lcaDist(Node root, int n) {
+        if(root == null) {
+            return -1;
+        }
+
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if(leftDist == -1) {
+            return rightDist+1;
+        }else {
+            return leftDist+1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n2);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+    
     public static void main(String[] args) {
         /* 
          *       1
@@ -225,7 +368,7 @@ public class heightTree {
          root.left.left = new Node(4);
          root.left.right = new Node(5);
          root.right.left = new Node (6);
-        //  root.right.right = new Node(7);
+         root.right.right = new Node(7);
 
         //  System.out.println(height(root));
 
@@ -255,7 +398,32 @@ public class heightTree {
 
 
 
-        topView(root);
+        // topView(root);
+
+
+
+        // int k = 3;
+        // KLevel(root, 1, k);
+
+
+
+
+
+        // int n1 = 4, n2 =5;
+        // System.out.println(lca(root, n1, n2).data);
+
+
+
+
+
+        // int n1 = 4, n2 = 6;
+        // System.out.println(lca2(root, n1, n2).data);
+
+
+
+        int n1 = 4, n2 = 5;
+        System.out.println(minDist(root, n1, n2));
+
 
     }
 }
