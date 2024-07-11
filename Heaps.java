2ac1637ed4caa3b771b37e869b89d57ec96c2b4d
Sumit Shah <sumitshah001122@gmail.com>
2024-07-11 21:01:05 +0545

    PQ for Objects.

--- Heaps.java
+++ Heaps.java
@@ -2,22 +2,59 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Heaps {
-    public static void main(String[] args) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());  //Reverse of Order-Descending Order
+    
+    // PQ in JCFf
+    // public static void main(String[] args) {
+    //     // PriorityQueue<Integer> pq = new PriorityQueue<>();
+    //     PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());  //Reverse of Order-Descending Order
 
 
-        pq.add(3); //0(logn)
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+    //     pq.add(3); //0(logn)
+    //     pq.add(4);
+    //     pq.add(1);
+    //     pq.add(7);
 
-        while(!pq.isEmpty()) {
-            System.out.println(pq.peek()); //0(1)
-            pq.remove(); //0(logn)
+    //     while(!pq.isEmpty()) {
+    //         System.out.println(pq.peek()); //0(1)
+    //         pq.remove(); //0(logn)
+    //     }
+
+
+    // }
+
+
+
+
+
+    //PQ FOR OBJECT
+    // Priority Queue for Student objects
+    static class Student implements Comparable<Student> { // Overriding
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
         }
 
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
 
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
+            pq.remove(); // O(log n)
+        }
     }
 
 }
