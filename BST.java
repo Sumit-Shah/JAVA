f4a1ac9227370c43a05559602469662c3675c634
Sumit-Shah <sumitshah001122@gmail.com>
2024-07-12 22:37:42 +0545

    book

--- BST.java
+++ BST.java
@@ -96,8 +96,33 @@ public class BST {
 
     }
 
+
+
+
+
+
+    // #######IMPORTANTG
+    //validation of binary seaerch tree 
+    public  static boolean isValidBST(Node root, Node min, Node max) {
+        if(root == null) {
+            return true;
+        }
+
+        if(min != null && root.data <= min.data) {
+            return false;
+        }
+
+        else if(max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, max)
+                && isValidBST(root.right, root, max);
+    }
+
     public static void main(String args []) {
-        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        int values[] = {1, 1, 1, 1, 1};
+        // int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
 
         for(int i=0; i<values.length; i++) {
@@ -113,11 +138,16 @@ public class BST {
         //     System.out.println("Not Found.");
         // }
         
-        root = delete(root, 1);
-        System.out.println();
-        inorder(root);
+        // root = delete(root, 1);
+        // System.out.println();
+        // inorder(root);
 
 
+        if(isValidBST(root, null, null)) {
+            System.out.println("VALID");
+        }else {
+            System.out.println("NOT VALID");
+        }
 
     }
     
