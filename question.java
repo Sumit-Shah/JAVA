e7688da2d89d6acd999d22f66d192cab24c31bc2
Sumit-Shah <sumitshah001122@gmail.com>
2024-10-04 09:10:23 +0545

    Starts with prefix problem

--- question.java
+++ question.java
@@ -22,15 +22,9 @@
 //         }
 
 //         }
-        
+
 //     }
 // }
-
-
-
-
-
-
 
 //Finding the car near by
 // import java.util.*;
@@ -54,8 +48,6 @@
 //         }
 //     }
 
-
-
 //     public static void main(String[] args) {
 //         int pts[][] = {{3,3}, {5,-1}, {-2,4}};
 //         int k = 2;
@@ -71,16 +63,8 @@
 //             System.out.println("C"+pq.remove().idx);
 //         }
 
-
 //     }
 // }
-
-
-
-
-
-
-
 
 //Connect n rope with minimum cost 
 // import java.util.*;
@@ -100,22 +84,12 @@
 //             int min2 = pq.remove();
 //             cost += min + min2;
 //             pq.add(min+min2);
-            
+
 //         }
 
 //         System.out.println("cost of connecting n ropes = " + cost);
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
 
 //WEAKEST SOLDIER
 // import java.util.*;
@@ -146,7 +120,7 @@
 //         int k=2;
 
 //         PriorityQueue<Row> pq = new PriorityQueue<>();
-        
+
 //         for(int i=0; i<army.length; i++) {
 //             int count = 0;
 //             for(int j=0; j<army[0].length; j++) {
@@ -160,12 +134,6 @@
 //         }
 //     }
 // }
-
-
-
-
-
-
 
 // //SLIDING WINDOWS MAXIMUM 
 // import java.util.*;
@@ -217,16 +185,6 @@
 // }
 // }
 
-
-
-
-
-
-
-
-
-
-
 //HASHING 
 //MAJORITY HASHING
 // import java.util.*;
@@ -254,7 +212,6 @@
 //         //     }
 //         // }
 
-
 //         for (Integer key : map.keySet()) { 
 //             if(map.get(key) > arr.length/3){
 //                 System.out.println(key);
@@ -262,10 +219,6 @@
 //         }
 //     }
 // }
-
-
-
-
 
 // VALID ANAGRAM
 // import java.util.*;
@@ -305,13 +258,6 @@
 //     }
 // }
 
-
-
-
-
-
-
-
 //COUNT DISTINCT ELEMENTS
 
 // import java.util.*;
@@ -320,12 +266,123 @@
 //         int num[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
 //         HashSet<Integer> set = new HashSet<>();
 
-
 //         for(int i=0; i<num.length; i++){
 //             set.add(num[i]);
 //         }
 
 //         System.out.println("ANS = " + set.size());
+//     }
+// }
+
+//UNION AND INTERSECTION of 2 arrays
+// import java.util.*;
+
+// import javax.lang.model.type.IntersectionType;
+// public class question{
+//     public static void main(String[] args) {
+//         int arr1[] = {7, 3, 9};
+//         int arr2[] = {6, 3, 9, 2, 9, 4};
+//         HashSet<Integer> set = new HashSet<>();
+
+//         //union
+//         for(int i=0; i<arr1.length; i++) {
+//             set.add(arr1[i]);
+//         }
+
+//         for(int i=0; i<arr2.length; i++){
+//             set.add(arr2[i]);
+//         }
+//         System.out.println("umion = " + set.size());
+
+//         // Intersection
+//         set.clear();
+//         for(int i=0; i<arr1.length; i++) {
+//             set.add(arr1[i]);
+//         }
+
+//         int count = 0;
+//         for(int i=0; i<arr2.length; i++) {
+//             if(set.contains(arr2[i])) {
+//                 count++;
+//                 set.remove(arr2[i]);
+//             }
+//         }
+
+//     }
+// }
+
+//find ITINERARY from Tickets   --- Topologicalsort
+// "Chennai" -> "Bengalru"
+// "Mumbai" -> "Delhi"
+// "Delhi" -> "Chennai"
+// "Delhi" -> "Goa"
+
+// "Mumbai" -> "Delhi" -> "Goa" -> "Chennai" -> "Benagluru"
+
+// import java.util.*;
+// public class question {
+//     public static String getStart(HashMap<String, String> tickets){
+//         HashMap<String, String> revMap = new HashMap<>();
+
+//         for(String key : tickets.keySet()) {
+//             revMap.put(tickets.get(key), key);
+//         }
+
+//         for(String key : tickets.keySet()) {
+//             if (!revMap.containsKey(key)) {
+//                 return key; //Starting Point
+//             }
+//         }
+
+//         return null;
+
+//     }
+//     public static void main(String[] args) {
+//         HashMap<String, String> tickets = new HashMap<>();
+//         tickets.put("Chennai", "Bengalru");
+//         tickets.put("Mumbai", "Delhi");
+//         tickets.put("Goa", "Chennai");
+//         tickets.put("Delhi", "Goa");
+
+//         String start = getStart(tickets);
+//         System.out.print(start);
+//         for(String key : tickets.keySet()) {
+//             System.out.print("->" + tickets.get(start));
+//             start = tickets.get(start);
+//         }
+
+//         System.out.println();
+//     }
+// }
+
+//LARGET SUB-ARRAY
+// import java.util.*;
+
+// public class question {
+//     public static void main(String[] args) { // O(n)
+//         int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23 };
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         // (sum, idx)
+
+//         int sum = 0;
+//         int len = 0;
+
+//         for (int j = 0; j < arr.length; j++) {
+//             sum += arr[j];
+
+//             if (sum == 0) {
+//                 len = j + 1;
+//             }
+
+//             if (map.containsKey(sum)) {
+//                 len = Math.max(len, j - map.get(sum));
+//             } else {
+//                 map.put(sum, j);
+//             }
+//         }
+
+//         System.out.println("Largest subarray with as 0 => " + len);
 //     }
 // }
 
@@ -337,40 +394,152 @@
 
 
 
-//UNION AND INTERSECTION of 2 arrays
-import java.util.*;
 
-import javax.lang.model.type.IntersectionType;
-public class question{
-    public static void main(String[] args) {
-        int arr1[] = {7, 3, 9};
-        int arr2[] = {6, 3, 9, 2, 9, 4};
-        HashSet<Integer> set = new HashSet<>();
 
-        //union
-        for(int i=0; i<arr1.length; i++) {
-            set.add(arr1[i]);
-        }
 
-        for(int i=0; i<arr2.length; i++){
-            set.add(arr2[i]);
-        }
-        System.out.println("umion = " + set.size());
-    
-        // Intersection
-        set.clear();
-        for(int i=0; i<arr1.length; i++) {
-            set.add(arr1[i]);
-        }
+// PREFIX PROBLEM
+// import java.util.*;
+// public class question {
+//     class Node {
+//         Node[] children = new Node[26];
+//         boolean eow = false;
+//         int freq;
 
-        int count = 0;
-        for(int i=0; i<arr2.length; i++) {
-            if(set.contains(arr2[i])) {
-                count++;
-                set.remove(arr2[i]);
+//         public Node() {
+//             for(int i=0; i<children.length; i++) {
+//                 children[i] = null;
+//             }
+//             freq = 1;
+//         }
+//         public static Node root = new Node();
+        
+//         public static void insert(String word) {
+//             Node curr = root;
+//             for(int i=0; i<word.length(); i++) {
+//                 int idx = word.charAt(i)-'a';
+//                 if(curr.children[idx] == null) { 
+//                     curr.children[idx] = new Node();
+//                 } else {
+//                     curr.children[idx].freq++;
+//                 }
+
+//                 curr = curr.children[idx];
+                
+//             }
+//             curr.eow = true;
+//         }
+//     }
+//     public static void findPrefix(Node root, String ans) {
+//         if(root == null) {
+//             return;
+//         }
+
+//         if(root.freq == 1) {
+//             System.out.println(ans);
+//             return;
+//         }
+//         for(int i=0; i<root.children.length;i++) {
+//             if(root.children[i] != null) {
+//                 findPrefix(root.children[i], ans+(char)(i+'a'));
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         String arr[] = {"zebra", "dog", "duck", "dove"};
+//         for(int i=0; i<arr.length; i++) {
+//             insert(arr[i]);  
+//         }
+
+//         root.freq = -1;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+//STARTS WITH PROBLEM
+// CREATE A FUNCTION BOOLEAN STARTS WITH (sTRING PREFIX) FOR A TRIE.
+// RETURNS TRUE IF THERE IS A PREVIOUSLY INSERTED STRING WORD THAT HAS THE PREFIX.  AND FALSE
+//OTHERWISE 
+
+// words[]  {"apple", "app", "mango", "man", "woman"}
+// prefix = "app" output: true
+//prefix = "moon" output: fasle
+
+public class question {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow;
+
+        public Node() {
+            for (int i=0; i<26; i++){
+                children[i] = null;
             }
         }
+    }
 
-    
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        int level = 0;
+        int len = word.length();
+        int idx = 0;
+
+        Node curr = root;
+        for(; level<len; level++) {
+            idx = word.charAt(level)-'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+
+    }
+
+    public static boolean search(String key) { //0(n)
+        int level = 0;
+        int len = key.length();
+        int idx = 0;
+
+        Node curr = root;
+        for(; level<len; level++) {
+            idx = key.charAt(level)-'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow = true;
+
+    }
+
+    public static boolean startsWith(String prefix){
+        Node curr = root;
+        for(int i=0; i<prefix.length(); i++) {
+            int idx = prefix.charAt(i)  - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = {"apple", "app", "mango", "man", "woman"};
+        String prefix1 = "app";
+        String prefix2 = "moon";
+
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+        System.out.println(prefix1);
+        System.out.println(prefix2);
     }
 }
